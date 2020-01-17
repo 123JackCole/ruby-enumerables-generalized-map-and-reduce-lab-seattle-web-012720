8189e7d7e722defbf383a8259d412be16176ca67
Jack Cole <123jack.cole@gmail.com>
2020-01-17 21:58:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,7 +14,7 @@ def reduce(source_array, starting_value = nil)
   end
   counter = 0
   while counter < source_array.length
-    total = yield(total, source_array[counter])
+    total += yield(source_array[counter])
     counter += 1
   end
   total
