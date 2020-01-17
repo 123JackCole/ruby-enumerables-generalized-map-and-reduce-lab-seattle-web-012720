043f90cb19887e5ae074dc4432d33563910a49af
Jack Cole <123jack.cole@gmail.com>
2020-01-17 21:06:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
-# Your Code Here
+def map(source_array)
+  new_array = []
+  counter = 0
+  while counter < source_array.length
+    new_array[counter] = yield 
+    counter += 1
+  end
+  new_array
+end
