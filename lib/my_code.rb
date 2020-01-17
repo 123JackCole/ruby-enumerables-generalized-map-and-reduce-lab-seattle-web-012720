7c38a0bc32bdfa2223cbb8d6c78ce64dcf054a4c
Jack Cole <123jack.cole@gmail.com>
2020-01-17 21:11:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,8 +2,7 @@ def map(source_array)
   new_array = []
   counter = 0
   while counter < source_array.length
-    yield source_array[counter]
-    new_array[counter] = source_array[counter]
+    new_array[counter] = yield(source_array[counter])
     counter += 1
   end
   new_array
